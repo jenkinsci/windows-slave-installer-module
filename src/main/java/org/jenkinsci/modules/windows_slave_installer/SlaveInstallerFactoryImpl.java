00a2f7f7fd8b9591b41d70aeecab3073927bd2c6
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-05-31 23:10:34 -0700

    Updated to work with newer core

--- src/main/java/org/jenkinsci/modules/windows_slave_installer/SlaveInstallerFactoryImpl.java
+++ src/main/java/org/jenkinsci/modules/windows_slave_installer/SlaveInstallerFactoryImpl.java
@@ -1,11 +1,8 @@
 package org.jenkinsci.modules.windows_slave_installer;
 
 import hudson.Extension;
-import hudson.Util;
-import hudson.remoting.Callable;
 import hudson.remoting.Channel;
-import hudson.slaves.SlaveComputer;
-import org.apache.commons.codec.binary.Base64;
+import jenkins.security.MasterToSlaveCallable;
 import org.jenkinsci.main.modules.instance_identity.InstanceIdentity;
 import org.jenkinsci.modules.slave_installer.SlaveInstaller;
 import org.jenkinsci.modules.slave_installer.SlaveInstallerFactory;
@@ -13,7 +10,6 @@ import org.jenkinsci.modules.slave_installer.SlaveInstallerFactory;
 import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
-import java.security.interfaces.RSAPublicKey;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -30,7 +26,7 @@ public class SlaveInstallerFactoryImpl extends SlaveInstallerFactory {
         return null;
     }
 
-    private static final class IsWindows implements Callable<Boolean,IOException> {
+    private static final class IsWindows extends MasterToSlaveCallable<Boolean,IOException> {
         public Boolean call() throws IOException {
             return File.pathSeparatorChar==';';
         }
