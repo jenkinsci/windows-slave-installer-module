78abe698a79e0032c89e3d3bc93bda993f379047
Oleg Nenashev <o.v.nenashev@gmail.com>
2017-02-24 18:19:12 +0100

    [JENKINS-39237] - Enable the automatic agent download during Windows Service Setup
    This change enables download of slave.jar on startup when it is possible.
    WindowsSlaveInstaller internal API has been slightly tweaked for testing purposes.

---
+++ src/test/java/org/jenkinsci/modules/windows_slave_installer/WindowsSlaveInstallerTest.java
@@ -0,0 +1,119 @@
+/**
+The MIT License
+
+Copyright (c) 2017 CloudBees, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+package org.jenkinsci.modules.windows_slave_installer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import org.apache.commons.io.IOUtils;
+import static org.hamcrest.CoreMatchers.*;
+import org.jenkinsci.modules.slave_installer.InstallationException;
+import org.jenkinsci.modules.slave_installer.LaunchConfiguration;
+import org.jenkinsci.modules.slave_installer.Prompter;
+import org.junit.Assert;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.jvnet.hudson.test.Issue;
+
+/**
+ * Tests of {@link WindowsSlaveInstaller}.
+ * @author Oleg Nenashev
+ */
+public class WindowsSlaveInstallerTest {
+    
+    @Rule
+    public TemporaryFolder tmpDir = new TemporaryFolder();
+    
+    private LaunchConfiguration launchConfig;
+    private Prompter prompter;
+    
+    @Before
+    public void initAttributes() throws IOException {
+        File storage = tmpDir.newFolder("agentDir");
+        File remotingJar = tmpDir.newFile("remoting.notjar");
+        
+        launchConfig = new MockLaunchConfiguration(
+                new URL("http://my.jenkins/jnlpJars/slave.jar"),
+                new URL("http://my.jenkins/computer/myAgent/connect.jnlp"),
+                storage, remotingJar
+        );
+        
+        prompter = new Prompter() {
+            @Override
+            public String prompt(String question, String defaultValue) throws InterruptedException {
+                return "unspecified";
+            }
+
+            @Override
+            public String promptPassword(String question) throws InterruptedException {
+                return "unspecified";
+            }
+        };
+    }
+    
+    @Test
+    @Issue("JENKINS-39237")
+    public void shouldGenerateConfigWithValidDownloadLink() throws InstallationException, IOException, InterruptedException {
+        
+        WindowsSlaveInstaller installer = new WindowsSlaveInstaller();
+        installer.install(launchConfig, prompter, true);
+        
+        final String xml;
+        try (FileInputStream istream = new FileInputStream(new File(tmpDir.getRoot(), "agentDir/jenkins-slave.xml"))) {
+            xml = IOUtils.toString(istream);
+        }
+        
+        // Verify that the directory has been initialized correctly
+        verifyAgentDirectory(new File(tmpDir.getRoot(), "agentDir"));
+        
+        // Check config.xml entries
+        assertThat("There is unresolved macro", xml, not(containsString("@")));
+        assertThat("The JAR download URL has not been resolved correctly", not(containsString("TODO:jarFile")));
+        assertThat("The JAR download URL contains the invalid value", xml, containsString("<download from=\"" + launchConfig.getLatestJarURL() + "\""));
+    }
+    
+    private static void verifyAgentDirectory(File dir) throws AssertionError {
+        assertAgentFile(dir, "jenkins-slave.exe");
+        assertAgentFile(dir, "jenkins-slave.exe.config");
+        assertAgentFile(dir, "jenkins-slave.xml");
+        assertAgentFile(dir, "slave.jar");
+    }
+    
+    private static void assertAgentFile(File parent, String child) throws AssertionError {
+        File file = new File(parent, child);
+        if (!file.exists()) {
+            Assert.fail("File does not exist: " + file);
+        }
+        if (!file.isFile()) {
+            Assert.fail("File is not a normal file: " + file);
+        }
+    }
+    
+    // TODO: Add more tests
+}
