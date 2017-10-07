c1deb5063d6599401c615ebd5fc11ff4da6fac19
Elton Alves <eltim.alves@gmail.com>
2017-10-06 23:46:33 -0300

    Update SlaveExeUpdater.java

--- src/main/java/org/jenkinsci/modules/windows_slave_installer/SlaveExeUpdater.java
+++ src/main/java/org/jenkinsci/modules/windows_slave_installer/SlaveExeUpdater.java
@@ -51,7 +51,7 @@ public class SlaveExeUpdater extends ComputerListener {
         final SlaveComputer sc = (SlaveComputer) c;
         final Boolean isUnix = sc.isUnix();
 
-        if(!isUnix) return;
+        if(isUnix) return;
 
 
         // do this asynchronously so as not to block Jenkins from using the slave right away
